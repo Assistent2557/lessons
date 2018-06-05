19e1c72e5e401fc349fe94698fc57eb5d4c8a293
Ershov Artem <assistent_2557@mail.ru>
2018-06-05 17:16:38 +0300

    Создал IF

--- lern2/if1.py
+++ lern2/if1.py
@@ -1,6 +1,17 @@
-if team == 'mystic':
-    print('Hi, blue team')
-elif team == 'instinct':
-    print('Hi, yellow team')
-elif team == 'valor':
-    print('Hi, red team')
+print("Введите возраст")
+age = int(input())
+
+if age == 0:
+   print("Вы не роделись")
+elif 1 < age <= 7:
+   print("Ходите в сад или ясли")
+elif 7 < age <= 17:
+   print("Учитесь в школе")
+elif 17 < age <= 23:
+   print("Учитесь в ВУЗЕ")
+elif 23 < age <= 65:
+   print("Работаете")
+elif 65 < age <= 100:
+   print("Вы пенсионер")
+else:
+   print(age)
